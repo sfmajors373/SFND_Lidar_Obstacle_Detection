c3f24ae0d373ce3a2551c971c163cf51b6d788c3
Sarah Majors <sfmajors373@gmail.com>
2023-08-02 16:40:21 +0000

    city block function in place

--- src/environment.cpp
+++ src/environment.cpp
@@ -90,6 +90,17 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
 }
 
 
+void cityBlock (pcl::visualization::PCLVisualizer::Ptr& viewer)
+{
+    // ------------------------------------------------
+    // ---- Open 3D viewer and display city block -----
+    // ------------------------------------------------
+
+    ProcessPointClouds<pcl::PointXYZI>* pointProcessor = new ProcessPointClouds<pcl::PointXYZI>();
+    pcl::PointCloud<pcl::PointXYZI>::Ptr inputCloud = pointProcessor->loadPcd("../src/sensors/data/pcd/data_1/0000000000.pcd");
+    renderPointCloud(viewer, inputCloud, "inputCloud");   
+}
+
 //setAngle: SWITCH CAMERA ANGLE {XY, TopDown, Side, FPS}
 void initCamera(CameraAngle setAngle, pcl::visualization::PCLVisualizer::Ptr& viewer)
 {
@@ -121,7 +132,7 @@ int main (int argc, char** argv)
     pcl::visualization::PCLVisualizer::Ptr viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
     CameraAngle setAngle = XY;
     initCamera(setAngle, viewer);
-    simpleHighway(viewer);
+    cityBlock(viewer);
 
     while (!viewer->wasStopped ())
     {
